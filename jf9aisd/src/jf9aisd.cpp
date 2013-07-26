1b4bfbf9bd54cf116611f223b962c8b327de2ec7
jijijijijiji <jijijijijiji@kilika.dyndns.org>
2013-07-26 19:52:04 +0200

    prova di commit ancora una volta

--- jf9aisd/src/jf9aisd.cpp
+++ jf9aisd/src/jf9aisd.cpp
@@ -12,5 +12,6 @@ using namespace std;
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout << "!!!versione 3!!!" << endl; // prints !!!Hello World!!!
+	cout << "commit numero 3"<< endl;
 	return 0;
 }
